821202a882c0423017a189eb46ccf71dc3480229
felipe-alvarenga <felipe.alvarenga@unifesp.br>
2024-11-10 14:53:11 -0300

    Create 1004.c

---
+++ 1004.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    int a, b, PRODUCT;
+    printf ("");
+    scanf ("%i", &a);
+    printf ("");
+    scanf ("%i", &b);
+    
+    PRODUCT = a * b;
+    
+    printf ("PROD = %i\n", PRODUCT);
+}
