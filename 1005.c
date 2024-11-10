94ecb3da83a04867111906a48d601d45e1ddbb58
felipe-alvarenga <felipe.alvarenga@unifesp.br>
2024-11-10 14:50:08 -0300

    Create 1005.c

---
+++ 1005.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    double a, b, c;
+    printf ("");
+    scanf ("%lf", &a);
+    printf ("");
+    scanf ("%lf", &b);
+    c = ((a*3.5)+(b*7.5))/11;
+    printf ("MEDIA = %.5lf\n", c);
+}
