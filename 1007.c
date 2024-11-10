4d94177b687b000ad4186f15364781bc07dbcef4
felipe-alvarenga <felipe.alvarenga@unifesp.br>
2024-11-10 14:48:49 -0300

    Create 1007.c

---
+++ 1007.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+  int a, b, c, d, J;
+  printf ("");
+  scanf ("%i", &a);
+  printf ("");
+  scanf ("%i", &b);
+  printf ("");
+  scanf ("%i", &c);
+  printf ("");
+  scanf ("%i", &d);
+  J = (a*b) - (c*d);
+  printf ("DIFERENCA = %i\n", J);
+}
