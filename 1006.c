fbfa13d7bef0f990b1c72d347522681512265462
felipe-alvarenga <felipe.alvarenga@unifesp.br>
2024-11-10 14:56:54 -0300

    Create 1006.c

---
+++ 1006.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    double a, b, c, d;
+    printf ("");
+    scanf ("%lf", &a);
+    printf ("");
+    scanf ("%lf", &b);
+    printf ("");
+    scanf ("%lf", &d);
+    c = ((a*2)+(b*3) + (d*5))/10;
+    printf ("MEDIA = %.1lf\n", c);
+}
