a68e633d599f77aeb3592fd259eb4bdd825c3285
felipe-alvarenga <felipe.alvarenga@unifesp.br>
2024-11-10 14:44:16 -0300

    Create 1014.c

---
+++ 1014.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    int a;
+    double b, c;
+    printf ("");
+    scanf ("%i", &a);
+    printf ("");
+    scanf ("%lf", &b);
+    c = a/b;
+    printf ("%.3lf km/l\n", c);
+}
