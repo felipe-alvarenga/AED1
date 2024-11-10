a38a6b281d414a46fece37fc9f7d30d63202517c
felipe-alvarenga <felipe.alvarenga@unifesp.br>
2024-11-10 14:51:39 -0300

    Create 1003.c

---
+++ 1003.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    int a, b, soma;
+    printf ("");
+    scanf ("%i", &a);
+    printf ("");
+    scanf ("%i", &b);
+    
+    soma = a + b;
+    
+    printf ("SOMA = %i\n", soma);
+}
